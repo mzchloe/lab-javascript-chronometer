658db6c4d1211a5a2c8d59ae2c9feaf6a0bb1a45
Jeanet Lin <linett@gmail.com>
2021-12-02 13:12:35 +0100

    chronometer.js all iterations complete

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -9,23 +9,22 @@ class Chronometer {
 
   start(callback) {
     
-    this.intervalId = setInterval(() => {
+   this.intervalId = setInterval(() => {
       this.currentTime += 1;
       if(callback){
         callback(); // so it does not throw an error in case it doesn't receive a function
       }
     }, 1000); //use of arrow function to enable referencing back to the object in the chronometer, otherwise it will refer to a global
-
-   
+ 
   }
 
   getMinutes() {
-    return Number(Math.floor((this.currentTime/60)));
+    return Number(Math.floor((this.currentTime/60))); 
   } //to get the number of full minutes, divide the number of total seconds by 60
   //currentTime / 60, and use math.floor to round the number to an integer
 
   getSeconds() {
-    return Number(Math.floor((this.currentTime%60)));
+  return Number(Math.floor((this.currentTime%60))); 
   } // remaining seconds = seconds % 60 
 
 
@@ -35,25 +34,27 @@ class Chronometer {
   // 2. If the number is less than 10, we add a 0 in front
   // --> if(value.toString < 10 ){
   //    value = '0' + valueToString
-  }
+  
+
   computeTwoDigitNumber(value) {
     
-    return value > 9 ? `${value}` : `0${value}`
+    return value > 9 ? `${value}` : `0${value}` 
     
   }
 
   stop() {
-    clearInterval(this.intervalId); //stops the interval
+    clearInterval(this.intervalId); //stops the interval 
   }
 
   reset() {
-    this.currentTime=0; //starts from 0 again when reset
+    this.currentTime=0; //starts from 0 again when reset 
   }
 
   split() {
     return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
-
+ 
   }
+}
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
